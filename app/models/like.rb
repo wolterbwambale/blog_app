8fac17ef4dcb11da449b390207e24d3e44a48ac3
wolterbwambale <benplaan1@gmail.com>
2023-10-26 01:11:50 +0300

    add methods to kike model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,13 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, foreign_key: :user_id
+  belongs_to :post, foreign_key: :post_id
+
+  # Method to update the likes counter for a post
+  after_save :update_post_likes_counter
+
+  private
+
+  def update_post_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
