484b34548fe55175bdb5958e85633b96052183a9
wolterbwambale <benplaan1@gmail.com>
2023-10-26 01:19:54 +0300

    fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    has_many :posts, foreign_key: 'author_id'
-    has_many :comments, foreign_key: 'user_id'
-    has_many :likes, foreign_key: 'user_id'
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'user_id'
+  has_many :likes, foreign_key: 'user_id'
 
-    # Method to get the 3 most recent posts for a user
+  # Method to get the 3 most recent posts for a user
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
