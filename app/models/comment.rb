28de55de9a1332178787e17c99e6213d8922708e
wolterbwambale <benplaan1@gmail.com>
2023-10-26 13:09:38 +0300

    fix the required changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
-  after_save :update_post_comments_counter
+  after_save :update_post_comment_counter
 
   private
 
-  def update_post_comments_counter
+  def update_post_comment_counter
     post.increment!(:comments_counter)
   end
 end
