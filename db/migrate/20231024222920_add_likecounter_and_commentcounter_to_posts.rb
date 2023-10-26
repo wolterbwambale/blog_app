35b3898b9b6bc510cf77a0d67199a0aa50d41615
wolterbwambale <benplaan1@gmail.com>
2023-10-26 13:08:31 +0300

    modify the likes migration

--- db/migrate/20231024222920_add_likecounter_and_commentcounter_to_posts.rb
+++ db/migrate/20231024222920_add_likecounter_and_commentcounter_to_posts.rb
@@ -1,6 +1,6 @@
 class AddLikecounterAndCommentcounterToPosts < ActiveRecord::Migration[7.0]
   def change
     add_column :posts, :likecounter, :integer
-    add_column :posts, :commentcounter, :integer
+    add_column :posts, :comments_counter, :integer
   end
 end
