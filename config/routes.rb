25d540b53cece1d60f6757c457df39a9edd9931f
wolterbwambale <benplaan1@gmail.com>
2023-11-01 10:28:57 +0300

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resource :users, only: [:index, :show] do
-    resource :posts, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
 end
