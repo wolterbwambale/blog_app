3b2fc4c28b4cc8bacf1bf74ed31a3e1395085fb4
wolterbwambale <benplaan1@gmail.com>
2023-11-02 01:50:53 +0300

    modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'user_id'
-  has_many :likes, foreign_key: 'user_id'
+  has_many :comments
+  has_many :likes
 
-  validates :name, presence: true
+  validates_presence_of :name
   validates_numericality_of :posts_counter, only_integer: true, greater_than_or_equal_to: 0
 
   # Method to get the 3 most recent posts for a user
