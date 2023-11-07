9f7b7dca1f7b1f4d7413f019d79c0f77ffb51b27
wolterbwambale <benplaan1@gmail.com>
2023-11-07 16:50:53 +0300

    modify the post for solve foreign conflicts

--- db/migrate/20231024221424_create_posts.rb
+++ db/migrate/20231024221424_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.references :author, null: false, foreign_key: { to_table: :users}
+      t.references :author,index: true, foreign_key: { to_table: :users }
 
       t.timestamps
     end
