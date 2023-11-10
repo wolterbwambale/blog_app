a5475b50f7d6cb5fb8bde44b0d2b1ed31a48cd06
wolterbwambale <benplaan1@gmail.com>
2023-11-10 14:53:28 +0300

    update the route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  root 'users#index'  
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index,:new,:create,:show]do
+    resources :posts, only: [:index,:new,:create,:show]
+  end
+
+  resources :posts do
     resources :comments, only: [:new, :create]
     resources :likes, only: [:create]
   end
+
+  root 'users#index'
 end
-end 
