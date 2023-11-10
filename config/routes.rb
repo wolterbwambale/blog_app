ab1e5bd282e3c71f96ca51e2bb0d77d8ab3e5601
wolterbwambale <benplaan1@gmail.com>
2023-11-10 13:05:32 +0300

    fix required changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index,:new,:create,:show]
-  end
 
-  resources :posts do
+  root 'users#index'  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index,:new,:create,:show]do
     resources :comments, only: [:new, :create]
     resources :likes, only: [:create]
   end
-
-  root 'users#index'
 end
+end 
